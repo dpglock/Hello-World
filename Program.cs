8edd96ca153088014154475164379fe65c612380
dpglock <Rewly1970@dayrep.com>
2016-03-21 12:55:39 +0100

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Beep(1000, 1500);
+            Console.WriteLine("Hello W0rld");
+            Console.ReadKey();
+        }
+    }
+}
